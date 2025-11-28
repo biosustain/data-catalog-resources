e41c4cd971232ef98bd40e2fe37071d31f71d4e6
Vasileia Vagena <110908916+VasileiaVagena@users.noreply.github.com>
2025-11-28 16:24:12 +0100

    Change  in the conf.py

--- conf.py
+++ conf.py
@@ -11,7 +11,7 @@ import subprocess
 
 project = "Data Catalog User Guide"
 copyright = "2025, Research Data Management, DTU BRIGHT"
-author = "Ding He"
+author = "Ding He, Vasileia Vagena"
 
 # -- Version information -----------------------------------------------------
 
